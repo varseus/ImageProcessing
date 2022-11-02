e2d386096343560e7a911bfab86e4aa189ed83a1
gongyueru <gong.yuer@northeastern.edu>
2022-11-01 22:23:32 -0400

    New Tests

--- test/TestModel.java
+++ test/TestModel.java
@@ -1,7 +1,17 @@
+import imageprocessing.controller.ImageProcessingController;
+import imageprocessing.model.Image;
+import imageprocessing.model.ImageProcessingModel;
+import imageprocessing.model.Pixel;
+import imageprocessing.view.TextScriptedImageProcessingView;
 import org.junit.Test;
 
 public class TestModel {
 
+private ImageProcessingModel model;
+private Pixel pixel;
+private TextScriptedImageProcessingView view;
+private ImageProcessingController controller;
+private Image image;
 
   @Test
   public void test(){
